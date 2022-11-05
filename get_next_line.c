5b1fd3fb926ae16f40b7150b5e996aae4b7a2ba7
Yassir El Massafi <yel-mass@e1r2p3.1337.ma>
2022-11-05 21:49:34 +0100

    test

--- get_next_line.c
+++ get_next_line.c
@@ -19,37 +19,52 @@ char *get_next_line(int fd)
     static char *buff = NULL;
     static char tmp[BUFFER_SIZE + 1];
     int i = 0;
+    int count2;
     int count = read(fd, tmp, BUFFER_SIZE);
+    if (count <= 0)
+        return (NULL);
     int stope = 1;
     tmp[BUFFER_SIZE] = '\0';
     if (buff != NULL)
         buff = ft_strjoin(buff, tmp);
-    else
+    else if (buff == NULL)
         buff = ft_strdup(tmp);
     while (stope)
     {
         i = ft_strchr(buff, '\n');
-        if (i != -1)
+        count += count2;
+        if (i != -1 || count2 == 0)
         {
-            char *s1 = ft_substr(buff, 0, i);
-            buff = ft_substr(buff, i + 1, count - i);
+            char *s1;
+            printf("%d\n", count);
+            printf("%s\n", buff);
+            if (i == -1)
+                s1 = ft_substr(buff, 0, count - 1);
+            else
+                s1 = ft_substr(buff, 0, i + 1);
+            buff = ft_substr(buff, i + 1, count - i + 1);
             return (s1);
         }
-        count += read(fd, tmp, BUFFER_SIZE) + 1;
-        buff = ft_strjoin(buff, tmp);
+        count2 = read(fd, tmp, BUFFER_SIZE);
+        if (count2 < 0)
+            return (NULL);
+        if (count != 0)
+            buff = ft_strjoin(buff, tmp);
+        printf("%s\n", buff);
     }
-    return ("(null)");
+
+    return (NULL);
 }
 
-// int main()
-// {
-//     int fd = open("text.txt", O_RDONLY);
+int main()
+{
+    int fd = open("files/41_no_nl", O_RDWR);
 
-//     printf("line1 = %s\n", get_next_line(fd));
-//     printf("\n\n\n");
-//     printf("line2 = %s\n", get_next_line(fd));
+    printf("fd = %d\nline1 = |%s|\n", fd, get_next_line(fd));
+    printf("\n\n\n");
+    printf("line2 = %s\n", get_next_line(fd));
 //     printf("\n\n\n");
 //     printf("line3 = %s\n", get_next_line(fd));
 //     printf("\n\n\n");
 //     printf("line4 = %s\n", get_next_line(fd));
-// }
+}
